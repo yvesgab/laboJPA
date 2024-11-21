ddf3b948020da77f634b12f4d501923cae34f8b1
yga <yves.gabriel@nsi-sa.be>
2024-11-21 15:47:21 +0100

    good unit test

--- src/test/java/be/nsi/labo/jpa/labojpa/LaboJpaTest.java
+++ src/test/java/be/nsi/labo/jpa/labojpa/LaboJpaTest.java
@@ -1,14 +1,5 @@
 package be.nsi.labo.jpa.labojpa;
 
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.fail;
-
 class LaboJpaTest {
-    @Test
-    void test() {
-        fail();
-    }
-
 }
